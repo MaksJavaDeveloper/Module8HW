b731f029213ac26379300f1639f739346283406e
MaksJavaDeveloper <maxikate170499@gmail.com>
2022-11-11 16:45:03 +0200

    add printNameClass

---
+++ src/main/java/Module8HW/PrintName.java
@@ -0,0 +1,10 @@
+package Module8HW;
+
+public class PrintName {
+
+    public void printName(Shape shape) {
+        System.out.println(shape.getName());
+    }
+
+
+}
--- src/main/java/Module8HW/ShapeTest.java
+++ src/main/java/Module8HW/ShapeTest.java
@@ -3,6 +3,7 @@ package Module8HW;
 public class ShapeTest {
     public static void main(String[] args) {
 
+
         Shape circle = new Circle();
         System.out.println("This shape is " + circle.getName());
         Shape cube = new Cube();
@@ -13,6 +14,16 @@ public class ShapeTest {
         System.out.println("This shape is " + oval.getName());
         Shape quad = new Quad();
         System.out.println("This shape is " + quad.getName());
+
+        PrintName printName = new PrintName();
+        printName.printName(circle);
+        printName.printName(cylinder);
+        printName.printName(cube);
+        printName.printName(oval);
+        printName.printName(quad);
+
+
+
 
     }
 }
